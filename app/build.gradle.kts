eff33c1b963baef09f059d42e398a24c679b231e
jbelardino <jbelardino@gmail.com>
2025-02-20 14:14:04 -0500

    push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
 }
